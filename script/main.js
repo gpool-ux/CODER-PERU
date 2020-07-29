8d32aa5f200120b4c4a9b3098668d2f8a9615663
GianPool <gpoolcaicedo@gmail.com>
2020-07-29 11:10:13 -0500

    parallax

--- script/main.js
+++ script/main.js
@@ -1,10 +1,18 @@
-//JS MENU RESPONSIVE
+////////////Parallax///////////
+var headerBg = document.getElementById('slider')
+window.addEventListener('scroll', function () {
+  headerBg.style.opacity = 1 - +window.pageYOffset / 550 + ''
+  headerBg.style.top = +window.pageYOffset / 800 + 'px'
+  headerBg.style.backgroundPositionY = +window.pageYOffset / 2 + 'px'
+})
+
+/////// MENU RESPONSIVE/////////////////
 function toggler() {
   var header = document.getElementById("header");
   header.classList.toggle("active");
 }
 
-//BOTON IR ARRIBA
+//////////BOTON IR ARRIBA/////////
 mybutton = document.getElementById("scrollUp");
 
 window.onscroll = function () {
@@ -23,3 +31,5 @@ function topFunction() {
   document.body.scrollTop = 0; // For Safari
   document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
 }
+
+/////////////////////////////////////////////////////////////////////////////////////////
